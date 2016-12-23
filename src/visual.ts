d0b9cb58bf5bbe603d766423482b73fdcc86ad6c
Maksim Galiev (Akvelon) <v-magal@microsoft.com>
2016-12-23 11:01:20 +0300

    added license label

--- src/visual.ts
+++ src/visual.ts
@@ -104,7 +104,8 @@ module powerbi.extensibility.visual {
 
             //add map tile
             var layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 18
+                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+                        maxZoom: 18
             }).addTo(this.map);
 
             this.connectionMapDataView = {
